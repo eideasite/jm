df9735e762d115b039cd92847c2bd58369494807
JeyamJM <jeyamohan@iykons.com>
2025-05-24 10:45:30 +0530

    Content Updated

--- src/components/home/skill/Skill.jsx
+++ src/components/home/skill/Skill.jsx
@@ -12,15 +12,16 @@ const Skill = ({ darkMode }) => {
       <div style={{ maxWidth: '1000px', margin: '0 auto' }}>
         <Space direction="vertical" size="large" style={{ width: '100%' }}>
           <Card
-            title="My Skillset"
+            title="T shaped Professional | Tech-Savvy | Business Analyst | Software Project Coordinator | Solution Analyst "
             bordered={false}
             className={`skill-card ${darkMode ? 'skill-card-dark' : 'skill-card-light'}`}
           >
             <Title level={3} style={{ color: darkMode ? '#fff' : '#000' }}>
-              Overview of My Skills
+             I BELIEVE IN MY TALENTS. 
             </Title>
             <p className={darkMode ? 'skill-text-dark' : 'skill-text-light'}>
-              Below are some of the essential hard and soft skills that define my expertise as a Web Developer.
+              I am JM — a tech-savvy Solution Analyst, Business Analyst, and Software Project Coordinator — skilled in leveraging modern technologies to drive innovation in scalable software solutions. I specialize in project planning, resource allocation, and ensuring timely product delivery while applying strategic thinking, analytical problem-solving, and risk management.
+              My expertise includes user story mapping, wireframe modeling, functional prototyping, and workflow optimization. With strong client communication and cross-team collaboration skills, I am prepared to contribute effectively to impactful projects in the modern IT sector.
             </p>
           </Card>
           <Card bordered={false} className={`skill-card ${darkMode ? 'skill-card-dark' : 'skill-card-light'}`}>
